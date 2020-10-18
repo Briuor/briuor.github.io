022852a7e52705bd69df6595c228d1165336270c
Briuor <briuorlol89@gmail.com>
2020-10-18 14:37:12 -0300

    Updates

---
+++ precache-manifest.795a1f9ab06dcaad25606685261911de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bdb0536308913617073ecf96f643f72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f189709f7fda80261adc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "686ed0ce85b7844b5fdc",
+    "url": "/static/js/2.3c4ad7ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3c4ad7ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f189709f7fda80261adc",
+    "url": "/static/js/main.bf27b7f8.chunk.js"
+  },
+  {
+    "revision": "4a73f2969bca9b9d24fc",
+    "url": "/static/js/runtime-main.e74af5f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
